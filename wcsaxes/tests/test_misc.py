4e2d7d6ac002e220c9b71da2a2bba096dd65928b
Edward Betts <edward@4angle.com>
2017-09-02 08:10:13 +0100

    correct spelling mistake

--- wcsaxes/tests/test_misc.py
+++ wcsaxes/tests/test_misc.py
@@ -17,7 +17,7 @@ from ..utils import get_coord_meta
 
 def test_grid_regression():
     # Regression test for a bug that meant that if the rc parameter
-    # axes.grid was set to True, WCSAxes would crash upon initalization.
+    # axes.grid was set to True, WCSAxes would crash upon initialization.
     plt.rc('axes', grid=True)
     fig = plt.figure(figsize=(3, 3))
     WCSAxes(fig, [0.1, 0.1, 0.8, 0.8])
